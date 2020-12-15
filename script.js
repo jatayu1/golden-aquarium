2a318c625f28e3609f81545bf74e21634991ebfa
akash das <60297810+jatayu1@users.noreply.github.com>
2020-12-15 14:41:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// JavaScript Document
+$(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+  });
+// setup nav
+const navBtn = document.getElementById("nav-btn");
+const navbar = document.getElementById("side-navbar");
+const navClose = document.getElementById("nav-close");
+// show nav
+navBtn.addEventListener("click", () => {
+  navbar.classList.add("showNav");
+});
+// close nav
+navClose.addEventListener("click", () => {
+  navbar.classList.remove("showNav");
+});
+
+
